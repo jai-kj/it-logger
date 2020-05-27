cc8dab46bc2defda7122d0b0fede1702975edd05
jaijain230999 <jai.j@somaiya.edu>
2020-05-27 10:00:50 +0530

    Deploy Ready

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 
@@ -9,11 +10,17 @@ connectDB()
 //Initialize Middleware
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => res.send('Welcome to IT Logger API...'))
-
 //Define Routes
 app.use('/api/logs', require('./routes/logs'))
 app.use('/api/techs', require('./routes/techs'))
+
+//Set Static Assets in Production
+if(process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('frontend/build'))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 5000
 
